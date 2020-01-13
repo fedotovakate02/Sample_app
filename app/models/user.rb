601216678386019bd15365c87524a92215da2c31
fedotovakate02 <fedotovakate02@gmail.com>
2020-01-13 21:58:05 +0300

    new

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-
-
+	VALID_EMAIL_REGEX = ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$
+	validates :name, presence:true, length: {maximum:5}
+	validates :email,	format: {with: /VALID_EMAIL_REGEX/ }
 end
