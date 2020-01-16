f13d4a6c770a18af4f352f6929066cad1aacd7ac
fedotovakate02 <fedotovakate02@gmail.com>
2020-01-16 21:47:03 +0300

    pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ApplicationRecord
-	VALID_EMAIL_REGEX = ^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$
-	validates :name, presence:true, length: {maximum:5}
-	validates :email,	format: {with: /VALID_EMAIL_REGEX/ }
+	VALID_EMAIL_REGEX = /\A\w+@\w+\.[a-z]+\z/i
+	before_save :to_lower_case
+	validates :name, presence:true, length: {maximum:50}
+	validates :email,	format: {with: VALID_EMAIL_REGEX },
+	          uniqueness: {case_sensitive: false} 
+
+has_secure_password	          
+	          
+private
+
+  def to_lower_case
+  self.email = email.downcase
+  end
 end
