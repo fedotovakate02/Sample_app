6ef45521164c1c0bd1e1271f244743f28a44d735
fedotovakate02 <fedotovakate02@gmail.com>
2020-01-23 22:06:10 +0300

    log

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ApplicationRecord
 	VALID_EMAIL_REGEX = /\A\w+@\w+\.[a-z]+\z/i
 	before_save :to_lower_case
-	validates :name, presence:true, length: {maximum:50}
-	validates :email,	format: {with: VALID_EMAIL_REGEX },
-	          uniqueness: {case_sensitive: false} 
+ validates :name, presence:true, length: {maximum:50}
+  validates :email,	format: {with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false} 
 
 has_secure_password	          
 	          
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'signup', to: 'users#new'
+  get 'sessions/new'
+  #get 'signup', to: 'users#new'
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  get 'users/show'
+ 
+  resources :users
+  resources :sessions, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
