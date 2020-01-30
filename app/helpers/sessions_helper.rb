513b62183a67390688406472d433eb25fecc29ff
fedotovakate02 <fedotovakate02@gmail.com>
2020-01-30 19:38:01 +0300

    update

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,9 @@
 module SessionsHelper
 	def current_user
-		if session[:user_id]
-			@current_user ||= User.find_by(id: session[:user_id])
-		elsif cookies.signed[:user_id]
-			user = User.find_by(id: cookies.signed[:user_id])
+		if (user_id = session[:user_id])
+			@current_user ||= User.find_by(id: user_id)
+		elsif (user_id = cookies.signed[:user_id])
+			user = User.find_by(id: user_id)
 			if user && user.authenticated?(cookies[:remember_token])
 				log_in user
 				@current_user = user
@@ -26,9 +26,16 @@ end
    end
 
    def log_out
+   	forget(current_user)
     session.delete(:user_id)
     @current_user = nil
    end
+
+   def forget(user)
+user.forget
+cookies.delete(:user_id)
+cookies.delete(:remember_token)
+end
 
 
 end
